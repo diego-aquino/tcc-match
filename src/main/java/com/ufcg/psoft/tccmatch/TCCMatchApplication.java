545c849f4a5b1e386c256d353804be29e2953989
Diego Aquino <diegocruzdeaquino@gmail.com>
2022-02-26 10:26:07 -0300

    style: format starter files with prettier

--- src/main/java/com/ufcg/psoft/tccmatch/TCCMatchApplication.java
+++ src/main/java/com/ufcg/psoft/tccmatch/TCCMatchApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TCCMatchApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TCCMatchApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(TCCMatchApplication.class, args);
+  }
 }
