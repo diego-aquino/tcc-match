b3ac9ac71b2b11d8459cf72a1b4ace1a5b5f4956
Diego Aquino <diegocruzdeaquino@gmail.com>
2022-03-04 20:45:50 -0300

    feat: create API exceptions

---
+++ src/main/java/com/ufcg/psoft/tccmatch/exceptions/api/ApiException.java
@@ -0,0 +1,8 @@
+package com.ufcg.psoft.tccmatch.exceptions.api;
+
+public class ApiException extends RuntimeException {
+
+  public ApiException(String message) {
+    super(message);
+  }
+}
---
+++ src/main/java/com/ufcg/psoft/tccmatch/exceptions/api/UnauthorizedApiException.java
@@ -0,0 +1,8 @@
+package com.ufcg.psoft.tccmatch.exceptions.api;
+
+public class UnauthorizedApiException extends ApiException {
+
+  public UnauthorizedApiException(String message) {
+    super(message);
+  }
+}
