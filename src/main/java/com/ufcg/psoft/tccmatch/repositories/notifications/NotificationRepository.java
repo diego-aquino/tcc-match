83c1bd64a15d2e0a531307a104585bef2ef8c307
Diego Aquino <diegocruzdeaquino@gmail.com>
2022-03-17 09:02:11 -0300

    feat: list notifications sent to user

--- src/main/java/com/ufcg/psoft/tccmatch/repositories/notifications/NotificationRepository.java
+++ src/main/java/com/ufcg/psoft/tccmatch/repositories/notifications/NotificationRepository.java
@@ -1,8 +1,15 @@
 package com.ufcg.psoft.tccmatch.repositories.notifications;
 
 import com.ufcg.psoft.tccmatch.models.notifications.Notification;
+import com.ufcg.psoft.tccmatch.models.users.User;
+import java.util.List;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface NotificationRepository extends JpaRepository<Notification, Long> {}
+public interface NotificationRepository extends JpaRepository<Notification, Long> {
+  List<Notification> findAllBySentTo(User userSentTo);
+
+  List<Notification> findAllBySentTo(User userSentTo, Sort sort);
+}
