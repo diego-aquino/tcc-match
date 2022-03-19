877f2e29696780d4fc55ed933a2142e2c4f788f6
Murilo <murilo.castro@ccc.ufcg.edu.br>
2022-03-19 18:59:57 -0300

    feat: creates user response dto for a generalized createdBy response

--- src/main/java/com/ufcg/psoft/tccmatch/dto/tccGuidanceProblem/TCCGuidanceProblemResponseDTO.java
+++ src/main/java/com/ufcg/psoft/tccmatch/dto/tccGuidanceProblem/TCCGuidanceProblemResponseDTO.java
@@ -1,19 +1,23 @@
 package com.ufcg.psoft.tccmatch.dto.tccGuidanceProblem;
 
+import com.ufcg.psoft.tccmatch.dto.users.UserResponseDTO;
 import com.ufcg.psoft.tccmatch.models.tccGuidanceProblem.TCCGuidanceProblem;
 import com.ufcg.psoft.tccmatch.models.tccGuidanceProblem.TCCGuidanceProblem.Category;
+import com.ufcg.psoft.tccmatch.models.users.User;
 
 public class TCCGuidanceProblemResponseDTO {
   private Long id;
   private Category category;
   private String description;
-  private Long userWhoCreatedId;
+  private Long tccGuidanceId;
+  private User createdBy;
 
   public TCCGuidanceProblemResponseDTO(TCCGuidanceProblem tccGuidanceProblem) {
     this.id = tccGuidanceProblem.getId();
     this.category = tccGuidanceProblem.getCategory();
     this.description = tccGuidanceProblem.getDescription();
-    this.userWhoCreatedId = tccGuidanceProblem.getCreatedBy().getId();
+    this.tccGuidanceId = tccGuidanceProblem.getTCCGuidance().getId();
+    this.createdBy = tccGuidanceProblem.getCreatedBy();
   }
 
   public Long getId() {
@@ -28,7 +32,11 @@ public class TCCGuidanceProblemResponseDTO {
     return description;
   }
 
-  public Long getUserWhoCreatedId() {
-    return userWhoCreatedId;
+  public Long getTCCGuidanceId() {
+    return tccGuidanceId;
+  }
+
+  public UserResponseDTO getCreatedBy() {
+    return new UserResponseDTO(createdBy);
   }
 }
---
+++ src/main/java/com/ufcg/psoft/tccmatch/dto/users/UserResponseDTO.java
@@ -0,0 +1,34 @@
+package com.ufcg.psoft.tccmatch.dto.users;
+
+import com.ufcg.psoft.tccmatch.models.users.User;
+import com.ufcg.psoft.tccmatch.models.users.User.Type;
+
+public class UserResponseDTO {
+  private Long id;
+  private Type type;
+  private String email;
+  private String name;
+
+  public UserResponseDTO(User user) {
+    this.id = user.getId();
+    this.email = user.getEmail();
+    this.name = user.getName();
+    this.type = user.getType();
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public Type getType() {
+    return type;
+  }
+}
